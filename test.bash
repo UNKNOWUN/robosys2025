66c64b502e035293d1ebafab0d77e9798a893344
UNKNOWN <xming3.huang@gmail.com>
2025-10-30 13:03:18 +0900

    Support integer calculation

--- test.bash
+++ test.bash
@@ -8,8 +8,18 @@ ng () {
 
 res=0
 
+### NORMAL INPUT ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15.0 ] || ng "$LINENO"
+
+### STRANGE INPUT ###i
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]	  || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo  | ./plus)
+[ "$?" = 1 ]	  ||ng "$LINENO"
+[ "${out}" = "" ] ||ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit $res
